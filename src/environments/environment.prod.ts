54483b039c1e6aea60b0865814a1f860479ec071
jhamisruiz <65043219+jhamisruiz@users.noreply.github.com>
2025-07-09 19:56:46 -0500

    fix env prod ip8002

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  host: '146.190.166.20:8080', //'gateway.adminedic.com',
+  host: '146.190.166.20:8002', //'gateway.adminedic.com',
   wsHost: 'prueba/ws',
   //wsSSL: true,
   wsSSL: false,
 
   userName: 'admin',//'admin' \ 'kalva,
   userPass: '123',//'admin123' \ '123,
-  apiHost: '146.190.166.20:8080', //'gateway.adminedic.com',
+  apiHost: '146.190.166.20:8002', //'gateway.adminedic.com',
   apiVersion: 'v1',
   //apiSSL: true,
   apiSSL: false,
@@ -16,5 +16,5 @@ export const environment = {
   dataLayout: 'horizontal',// horizontal | vertical | semibox
 
   //backend public
-  hostPublic: '146.190.166.20:8080', //'https://gateway.adminedic.com',
+  hostPublic: '146.190.166.20:8002', //'https://gateway.adminedic.com',
 };
